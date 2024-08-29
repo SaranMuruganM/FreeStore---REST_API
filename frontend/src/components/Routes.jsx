e49a5fb12dd5033234d15ec7d5471221b760fb52
SaranMuruganM <msaran913@gmail.com>
2024-08-29 22:42:55 +0530

    routing config on Routes

--- frontend/src/components/Routes.jsx
+++ frontend/src/components/Routes.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React from "react";
 
 const Routes = () => {
+  const baseUrl = import.meta.env.VITE_API_BASE_URL;
+
   return (
     <div className="mt-[10vh] space-y-4 text-sm">
       <div className="text-xl font-semibold text-[#0A2E36] tracking-wide">
@@ -9,24 +11,22 @@ const Routes = () => {
       <div className="opacity-55">All HTTP methods are supported</div>
       <div className="space-y-6 *:text-[#0A2E36] *:font-semibold *:justify-between *:flex-wrap">
         <div className="flex items-center">
-          <a href="http://localhost:5100/api/products">GET</a>
-          <a href="http://localhost:5100/api/products">/products</a>
+          <a href={`${baseUrl}/api/products`}>GET</a>
+          <a href={`${baseUrl}/api/products`}>/products</a>
         </div>
         <div className="flex items-center">
-          <a href="http://localhost:5100/api/products">GET</a>
-          <a href="http://localhost:5100/api/products/66cc3c5f3b9d635c2af94334">
+          <a href={`${baseUrl}/api/products/66cc3c5f3b9d635c2af94334`}>GET</a>
+          <a href={`${baseUrl}/api/products/66cc3c5f3b9d635c2af94334`}>
             /products/66cc3c5f3b9d635c2af94334
           </a>
         </div>
-        <div className="flex items-center ">
-          <a href="http://localhost:5100/api/products">GET</a>
-          <a href="http://localhost:5100/api/products/category">
-            /products/category
-          </a>
+        <div className="flex items-center">
+          <a href={`${baseUrl}/api/products/category`}>GET</a>
+          <a href={`${baseUrl}/api/products/category`}>/products/category</a>
         </div>
-        <div className="flex items-center ">
-          <a href="http://localhost:5100/api/products">GET</a>
-          <a href="http://localhost:5100/api/products/category/mobiles">
+        <div className="flex items-center">
+          <a href={`${baseUrl}/api/products/category/mobiles`}>GET</a>
+          <a href={`${baseUrl}/api/products/category/mobiles`}>
             /products/category/mobiles
           </a>
         </div>
@@ -36,6 +36,6 @@ const Routes = () => {
       </div>
     </div>
   );
-}
+};
 
-export default Routes
+export default Routes;
